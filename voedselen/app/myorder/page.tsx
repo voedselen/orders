27122b2c0de7d2f60d95ad74f5d1ead96228c855
Alexander <alexvanheteren@gmail.com>
2023-09-26 11:48:33 +0200

    pagina's aangemaakt en stylesheet gekoppeld

---
+++ voedselen/app/myorder/page.tsx
@@ -0,0 +1,8 @@
+import styles from './page.module.css';
+
+
+export default function MyOrder() {
+    return (
+        <h1>Myorder page</h1>
+    )
+};
---
+++ voedselen/app/reservation/page.tsx
@@ -0,0 +1,8 @@
+import styles from './page.module.css';
+
+
+export default function Reservation() {
+    return (
+        <h1>Reservation page</h1>
+    )
+};
