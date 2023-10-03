782d4ea877bd6a4ba3e883b3ea7e1eccf298940b
Aya <aya.dekok@student.fontys.nl>
2023-10-03 10:52:27 +0200

    sql code for saving into db

---
+++ Backend/VoedselBackend/BusinessLayer/MenuItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class MenuItem
+    {
+        private int id;
+        private string name;
+        private string description;
+        private double price;
+    }
+}
---
+++ Backend/VoedselBackend/DBLayer/DB_AccessOrder.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLayer
+{
+    public class DB_AccessOrder
+    {
+        private const string CONNECTION_STRING = "Server=mssqlstud.fhict.local;Database=dbi507545_voedseldb;User Id=dbi507545_voedseldb;Password=Voedsel1!;";
+        //add order into db
+        public bool AddOrderDB(Order order)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(CONNECTION_STRING);
+                connection.Open();
+                SqlCommand insertCommand = new SqlCommand("INSERT INTO Order (TotalPrice, TableNumber) " +
+                    "VALUES (@TotalPrice, @TableNumber)", connection);
+                insertCommand.Parameters.AddWithValue("@TotalPrice", order.TotalPrice);
+                insertCommand.Parameters.AddWithValue("@TableNumber", order.OrderTable);
+                insertCommand.ExecuteNonQuery();
+                connection.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+
+        }
+    }
+}
