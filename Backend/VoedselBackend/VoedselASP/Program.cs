5ecb802b0355b36d869237fc37f9332cac343004
Aya <aya.dekok@student.fontys.nl>
2023-10-31 11:43:41 +0100

    services addscoped

--- Backend/VoedselBackend/VoedselASP/Program.cs
+++ Backend/VoedselBackend/VoedselASP/Program.cs
@@ -1,6 +1,10 @@
+using BusinessLayer;
+using DBLayer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IDB_AccessOrder, DB_AccessOrder>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
