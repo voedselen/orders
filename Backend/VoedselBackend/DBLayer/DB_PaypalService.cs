b5b0fbd2748fa40f9f1b59a8086ca0547dca3a19
Dklaver <512957@student.fontys.nl>
2023-12-15 20:33:34 +0100

    Update paid working

---
+++ Backend/VoedselBackend/DBLayer/DB_PaypalService.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer.Classes;
+using BusinessLayer.DB_Interfaces;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLayer
+{
+    public class DB_PaypalService : IDB_PaypalService
+    {
+        private const string CONNECTION_STRING = "Server=mssqlstud.fhict.local;Database=dbi507545_voedseldb;User Id=dbi507545_voedseldb;Password=Voedsel1!;TrustServerCertificate=True;";
+        public bool UpdatePaymentStatus(Payments orderlist)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(CONNECTION_STRING);
+                connection.Open();
+
+                string listText = string.Join(',', orderlist.OrderIdList);
+                Console.Write(listText);
+
+                string query = $"UPDATE Orders SET paid = 1 WHERE id IN ({listText})";
+                SqlCommand updatePaidCommand = new SqlCommand(query, connection);
+
+                updatePaidCommand.ExecuteNonQuery();
+
+                connection.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+        }
+    }
+}
---
+++ Backend/VoedselBackend/VoedselASP/Controllers/PaymentController.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Classes;
+using BusinessLayer.DB_Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VoedselASP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IPaypalServices _paypalService;
+        public PaymentController(IPaypalServices iPaypalService)
+        {
+            _paypalService = iPaypalService;
+        }
+        [HttpPut]
+        
+        public IActionResult Update([FromBody] Payments orderList)
+        {
+            
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                _paypalService.UpdatePaymentStatus(orderList);
+
+                bool result = true;
+                var response = new { result = result, status = 200, message = "success" };
+                return Ok(response);
+                
+            }
+            catch (Exception ex)
+            {
+                bool result = false;
+                var response = new { result = result, status = 200, message = ex.Message };
+                return BadRequest(response);
+            }
+        }
+
+        // GET: PaymentController/Delete/5
+
+    }
+}
